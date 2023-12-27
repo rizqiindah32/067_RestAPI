cbeb1adda45704c38c9760f9c92b56205a5b0cdd
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-28 03:37:46 +0700

    menambahkan column pada function FormInputSiswa

--- app/src/main/java/com/example/praktikum8/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/praktikum8/ui/kontak/screen/InsertScreen.kt
@@ -1,8 +1,11 @@
 package com.example.praktikum8.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.praktikum8.ui.kontak.viewmodel.InsertUiEvent
 import com.example.praktikum8.ui.kontak.viewmodel.InsertUiState
 
@@ -14,5 +17,10 @@ fun FormInputSiswa(
     onValueChange: (InsertUiEvent)-> Unit = {},
     enable: Boolean = true
 ){
-    
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+
+    }
 }
