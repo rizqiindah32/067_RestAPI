3003a8ccfcb984dded51a7924e03e04b71c6797b
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-28 03:58:08 +0700

    membuat function EntryKontakBody

--- app/src/main/java/com/example/praktikum8/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/praktikum8/ui/kontak/screen/InsertScreen.kt
@@ -2,17 +2,33 @@ package com.example.praktikum8.ui.kontak.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.praktikum8.ui.kontak.viewmodel.InsertUiEvent
 import com.example.praktikum8.ui.kontak.viewmodel.InsertUiState
 
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputSiswa(
-    insertUiState: InsertUiState,
+    insertUiEvent: InsertUiEvent,
     modifier: Modifier = Modifier,
     onValueChange: (InsertUiEvent)-> Unit = {},
     enable: Boolean = true
@@ -21,6 +37,40 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ){
-
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama = it))},
+            label = { Text("Nama")},
+            modifier =  Modifier.fillMaxWidth(),
+            enable = enable,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.alamat,
+            onValueChange = {onValueChange(insertUiEvent.copy(alamat = it))},
+            label = { Text("Alamat")},
+            modifier = Modifier.fillMaxWidth(),
+            enable = enable,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.telpon ,
+            onValueChange = {onValueChange(insertUiEvent.copy(telpon = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("Telpon")},
+            modifier = Modifier.fillMaxWidth(),
+            enable = enable,
+            singleLine = true
+        )
+        if (enable){
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
     }
 }
--- app/src/main/java/com/example/praktikum8/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum8/ui/kontak/viewmodel/InsertViewModel.kt
@@ -2,6 +2,7 @@ package com.example.praktikum8.ui.kontak.viewmodel
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.praktikum8.model.Kontak
