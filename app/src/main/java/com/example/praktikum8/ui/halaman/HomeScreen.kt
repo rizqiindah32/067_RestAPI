57c50b3a922d09394c3738cc41f6c32d46db9dd5
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-12-28 01:34:42 +0700

    membuat function HomeScreen

--- app/src/main/java/com/example/praktikum8/ui/halaman/HomeScreen.kt
+++ app/src/main/java/com/example/praktikum8/ui/halaman/HomeScreen.kt
@@ -14,20 +14,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -36,6 +40,7 @@ import com.example.praktikum8.R
 import com.example.praktikum8.model.Kontak
 import com.example.praktikum8.ui.PenyediaViewModel
 import com.example.praktikum8.ui.TopAppBar
+import com.example.praktikum8.ui.TopAppBarKontak
 import com.example.praktikum8.ui.home.viewmodel.HomeViewModel
 import com.example.praktikum8.ui.home.viewmodel.KontakUIState
 
@@ -48,7 +53,35 @@ fun HomeScreen(
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ){
+              Icon(imageVector = Icons.Default.Add,
+                  contentDescription = "Add Kontak")
+            }
+        },
+    ){ innerPadding ->
+        HomeStatus(
+            kontakUIState = viewModel.kontakUIState,
+            retryAction = { viewModel.getKontak() },modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick, onDeleteClick = {
+                viewModel.deleteKontak(it.id)
+                viewModel.getKontak()
+            }
+        )
+    }
 }
 @Composable
 fun HomeStatus(
@@ -114,8 +147,8 @@ fun KontakLayout(
             Kontakcard(
                 kontak = kontak,
                 modifier = Modifier
-                .fillMaxWidth()
-                .clickable {onDetailClick(kontak)},
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(kontak) },
                 onDeleteClick = {
                     onDeleteClick(kontak)
                 }
